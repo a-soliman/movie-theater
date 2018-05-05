97d37343967cdd4e84f1b4230b0e4f02f88654a0
a-soliman <ahmed.soliman@programmer.net>
2018-05-05 13:56:42 -0700

    coded the movies list resource

--- server/resources/movies_list.py
+++ server/resources/movies_list.py
@@ -7,5 +7,23 @@ class Movies_List(Resource):
         ''' 
         returns a list of movies
         '''
-        return 'This should return a list of movies'
-    
+        movies = MovieModel.get_all_movies()
+
+        return { 'movies': [ movie.json() for movie in movies ] }
+
+
+
+# TEST DATA
+avengers_infinity_war = MovieModel(
+    'Avengers: Infinity War', 
+    'Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos. On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality.',
+    'http://t2.gstatic.com/images?q=tbn:ANd9GcQoBtRhueP0Kn_O7e89DXSBKBUz-1Nu4Ngb9eqFzqF3EbPGWYVP',
+    'https://youtu.be/6ZfuNTqbHE8'
+    )
+
+deadpool_2 = MovieModel(
+    'Deadpool 2',
+    'Wisecracking mercenary Deadpool joins forces with three mutants -- Bedlam, Shatterstar and Domino -- to protect a boy from the all-powerful Cable.',
+    'http://t2.gstatic.com/images?q=tbn:ANd9GcTkbXNbwGV0npOKCGSndE-YCGpRb2xQDRV8VyMfGlsEfej-sVMv',
+    'https://youtu.be/20bpjtCbCz0'
+    )
