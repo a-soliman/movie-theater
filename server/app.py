da7e75526aa0480e111fce25430c4b32daaee271
a-soliman <ahmed.soliman@programmer.net>
2018-05-04 22:35:11 -0700

    imported the resources class and ran an app test #200#

--- server/app.py
+++ server/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource
 
+from resources.movie import Movie
+from resources.movies_list import Movies_List
+
+
 app = Flask(__name__)
 api = Api(app)
 
+api.add_resource(Movies_List, '/')
+api.add_resource(Movie, '/<string:id>')
 
 if __name__ == '__main__':
     print('Working...')
     app.run(port=5000, debug=True)
-    
