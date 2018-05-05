8dc754731cb802d4f6c5b8acd0738f8ddf95fc6c
a-soliman <ahmed.soliman@programmer.net>
2018-05-04 22:21:08 -0700

    initialized the movie model class

---
+++ server/models/movie.py
@@ -0,0 +1,8 @@
+
+class MovieModel():
+    def __init__(self, title, story_line, poster, trailer_link):
+        self.title = title
+        self.story_line = story_line
+        self.poster = poster
+        self.trailer_link = trailer_link
+    
