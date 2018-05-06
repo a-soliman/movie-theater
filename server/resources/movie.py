2eae3e13d9376d457f6d7c446783f259c827155f
a-soliman <ahmed.soliman@programmer.net>
2018-05-06 09:36:16 -0700

    used reqparser from flask restful to parse the passed json data if any.

--- server/resources/movie.py
+++ server/resources/movie.py
@@ -1,8 +1,35 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparser
 
 from models.movie import MovieModel
 
 class Movie(Resource):
+    # the following will parse json data passed with the requist if any
+    parser = reqparser.RequestParser()
+
+    parser.add_argument('title', 
+        type = str,
+        required = True,
+        help = 'Movie title is required.'
+    )
+
+    parser.add_argument('story_line', 
+        type = str,
+        required = True,
+        help = 'Movie story-line is required.'
+    )
+
+    parser.add_argument('poster', 
+        type = str,
+        required = True,
+        help = 'a URL to the movie poster is required.'
+    )
+
+    parser.add_argument('trailer_link', 
+        type = str,
+        required = True,
+        help = 'a URL to Movie video trailer is required.'
+    )
+    
     def get(self):
         pass
     
