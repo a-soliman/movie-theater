11f365846b4005687deaba77563f4d16b7e9cb9b
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-05-07 09:16:08 -0700

    coded a sample for testing the Front end of the app

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,3 +1,16 @@
-var name = 'Ahmed';
 
+$(document).ready(() => {
+    
+    $('.movie').on('mouseenter', function() {
+        $(this).find('.details').stop().animate({
+            "top": "-115px"
+            
+        }, 200)
+    })
 
+    $('.movie').on('mouseleave', function() {
+        $(this).find('.details').stop().animate({
+            "top": "0px"
+        }, 200)
+    })
+})
