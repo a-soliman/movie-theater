ebe32cf1d36952c6621dbb64a691a455780fbc8f
a-soliman <ahmed.soliman@programmer.net>
2018-05-08 05:21:56 -0700

    coded a run file

---
+++ server/run.py
@@ -0,0 +1,9 @@
+from app import app
+from db import db
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
