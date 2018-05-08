fe6e12b7706f2bd2d889308d97fb6ac295f97cf8
a-soliman <ahmed.soliman@programmer.net>
2018-05-07 20:24:00 -0700

    added a cross origin rules

--- server/app.py
+++ server/app.py
@@ -4,10 +4,16 @@ from flask_restful import Api, Resource
 from resources.movie import Movie
 from resources.movies_list import Movies_List
 
-
 app = Flask(__name__)
 api = Api(app)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+    
 api.add_resource(Movies_List, '/')
 api.add_resource(Movie, '/<string:_id>')
 
