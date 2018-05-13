627526a3d1a7eebc2d0bacdc2c5346e30f83d118
a-soliman <ahmed.soliman@programmer.net>
2018-05-13 11:17:19 -0700

    minro edits as per PEP8 validation requirments

--- server/models/movie.py
+++ server/models/movie.py
@@ -1,17 +1,15 @@
 from db import db
 from bson.objectid import ObjectId
 
+
 class MovieModel():
     '''
-    This class acts as a blueprint for the movie object,
-    - it includes a set of methods to interact with the database performing CRUD operations
-    - it includes json() method that returns the JSON converted version of the movie object
-    
-    Notes: 
-        while performing POST request using the method 'save_to_db ()', make sure to pass None for the (_id) argument,
-        Py-Mongo will create an objectId for the movie as it saves the instance to the database
-    
-    Args: 
+    This class acts as a blueprint for the movie object.
+
+    Notes:
+        while using the method 'save_to_db ()', make sure to pass None for the (_id) argument.
+
+    Args:
         _id (str): a string of the _id, which will be converted to an objectId before matching in the database.
         title (str): represents the movie title.
         story_line (str): represents the movie's storyline.
@@ -39,10 +37,10 @@ class MovieModel():
         movies = []
 
         for movie in movies_collection.find():
-            movie = MovieModel( movie['_id'], movie['title'], movie['story_line'], movie['poster'], movie['trailer_link'])
+            movie = MovieModel(movie['_id'], movie['title'], movie['story_line'], movie['poster'], movie['trailer_link'])
             movies.append(movie)
         return movies
-    
+
     @classmethod
     def find_by_id(cls, _id):
         # Finds a movie by ID
@@ -53,11 +51,11 @@ class MovieModel():
             movie = MovieModel(result['_id'], result['title'], result['story_line'], result['poster'], result['trailer_link'])
         except:
             return None
-        
+
         return movie
 
     def json(self):
-        # returns a JSON formated version of the instance 
+        # returns a JSON formated version of the instance.
         return {
                 "_id": str(self._id),
                 "title": self.title,
@@ -65,22 +63,21 @@ class MovieModel():
                 "poster": self.poster,
                 "trailer_link": self.trailer_link
             }
-    
+
     def save_to_db(self):
         # saves to DB
         movies_collection = db.movies
         movie_id = movies_collection.insert({"title": self.title, "story_line": self.story_line, "poster": self.poster, "trailer_link": self.trailer_link})
         return movie_id
-    
+
     def delete_from_db(self):
         # deltes from DB
         movies_collection = db.movies
         result = movies_collection.remove({'_id': ObjectId(self._id)})
         return result
-    
+
     def update_in_db(self):
-        # updates a movie in the db 
+        # updates a movie in the db.
         movies_collection = db.movies
         movies_collection.update({'_id': ObjectId(self._id)}, {'$set': {"title": self.title, "story_line": self.story_line, "poster": self.poster, "trailer_link": self.trailer_link}})
-        return 
-        
+        return
--- server/resources/movie.py
+++ server/resources/movie.py
@@ -2,29 +2,30 @@ from flask_restful import Resource, reqparse
 
 from models.movie import MovieModel
 
+
 class Movie(Resource):
     # the following will parse json data passed with the requist if any
     parser = reqparse.RequestParser()
 
-    parser.add_argument('title', 
+    parser.add_argument('title',
         type = str,
         required = True,
         help = 'Movie title is required.'
     )
 
-    parser.add_argument('story_line', 
+    parser.add_argument('story_line',
         type = str,
         required = True,
         help = 'Movie story-line is required.'
     )
 
-    parser.add_argument('poster', 
+    parser.add_argument('poster',
         type = str,
         required = True,
         help = 'a URL to the movie poster is required.'
     )
 
-    parser.add_argument('trailer_link', 
+    parser.add_argument('trailer_link',
         type = str,
         required = True,
         help = 'a URL to Movie video trailer is required.'
@@ -33,12 +34,12 @@ class Movie(Resource):
     def get(self, _id):
         if not MovieModel.is_valid_id(_id):
             return {'message': 'Invaid Id'}, 400
-        
+
         movie = MovieModel.find_by_id(_id)
         if movie:
             return movie.json()
         return {'message': 'Not Found'}, 404
-    
+
     def post(self, _id):
         data = Movie.parser.parse_args()
         new_movie = MovieModel(None, data['title'], data['story_line'], data['poster'], data['trailer_link'])
@@ -48,36 +49,36 @@ class Movie(Resource):
         except:
             return {'message': 'An error has occurred'}, 500
         return {'message': 'New Movie has been inserted'}, 201
-    
+
     def put(self, _id):
         if not MovieModel.is_valid_id(_id):
             return {'message': 'Invaid Id'}, 400
-        
+
         movie = MovieModel.find_by_id(_id)
         if movie is None:
             return {'message': 'Movie was not found'}, 404
-        
-        #parse the updated passed data.
+
+        # parse the updated passed data.
         data = Movie.parser.parse_args()
         movie.title = data['title']
         movie.story_line = data['story_line']
         movie.poster = data['poster']
         movie.trailer_link = data['trailer_link']
 
-        try: 
+        try:
             movie.update_in_db()
         except:
             return {'message': 'An error has occurred'}, 500
         return {'message': 'Updated.'}, 200
-    
+
     def delete(self, _id):
         if not MovieModel.is_valid_id(_id):
             return {'message': 'Invaid Id'}, 400
-        
+
         movie = MovieModel.find_by_id(_id)
         if movie is None:
             return {'message': 'Movie was not found'}, 404
-        
+
         try:
             movie.delete_from_db()
         except:
